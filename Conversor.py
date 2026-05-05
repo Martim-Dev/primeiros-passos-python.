23c60c927323d535f3bf2a1a2c709231db81a6ca
Martim-Dev <marti28pc@gmail.com>
2026-05-05 15:48:58 +0100

    Add international currency converter in Conversor.py

--- Conversor.py
+++ Conversor.py
@@ -1,1 +1,19 @@
+print("--- CONVERSOR INTERNACIONAL ---")
+
+while True:
+    entrada = input("\nValor em Euros (ou 'sair'): ").replace(',', '.')
+    
+    if entrada.lower() == 'sair':
+        print("A fechar o programa...")
+        break
+        
+    try:
+        euros = float(entrada)
+        taxa_dolar = 1.08
+        
+        print(f"Em Dólares: ${euros * taxa_dolar:.2f}")
+        print(f"Na Finlândia: {euros:.2f}€")
+        
+    except ValueError:
+        print("Erro: Escreve apenas números (ex: 10.50)")
 
